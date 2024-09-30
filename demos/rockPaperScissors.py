57c0cb907d55cf5fe205e1cd2b2979869055f90c
metalBread33 <kfoughty80@gmail.com>
2024-09-30 13:14:22 -0400

    Add rockPaperScissors demo

---
+++ demos/rockPaperScissors.py
@@ -0,0 +1,14 @@
+
+computerChoice = 3
+userChoice = int(input('Rock, paper, or scissors? \n1) Rock\n2) Paper \n3) Scissors\n'))
+
+if computerChoice == userChoice:
+    print("TIE")
+elif userChoice == 1 and computerChoice == 3:
+    print("YOU WIN!")
+elif userChoice == 3 and computerChoice == 1:
+    print("YOU WIN!")
+elif userChoice == 2 and computerChoice == 1 :
+    print("YOU WIN!")
+else: 
+    print("You lose :(")
