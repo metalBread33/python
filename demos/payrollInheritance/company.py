5b7fb494daf1de28ece7357b2388a0e8c5a3d929
Kevin Foughty <97193605+metalBread33@users.noreply.github.com>
2024-10-02 11:14:41 +0000

    Ineritance

---
+++ demos/payrollInheritance/company.py
@@ -0,0 +1,38 @@
+from employee import Employee, SalaryEmployee, HourlyEmployee, ComissionEmployee
+
+class Company:
+    def __init__(self):
+        self.employees = []
+
+    def add_employee(self, new_employee):
+        self.employees.append(new_employee)
+
+    def display(self):
+        print('Current Employees:')
+        for i in self.employees:
+            print(i.fname, i.lname)
+        print('---------------------')
+
+    def pay_employees(self):
+        print('Paying Employees')
+        for i in self.employees:
+            print('Paycheck for:', i.fname, i.lname)
+            print(f'Amount: ${i.calculate_paycheck():,.2f}')
+            print('-----------------')
+
+def main():
+    my_company = Company()
+
+    employee = SalaryEmployee('Kevin', 'Foughty', 72561)
+    my_company.add_employee(employee)
+
+    hourly = HourlyEmployee('Jane', 'Doe', 40, 10)
+    my_company.add_employee(hourly)
+
+    com = ComissionEmployee('Sponge', 'Bob', 30000, 15, 100)
+    my_company.add_employee(com)
+
+    my_company.display()
+    my_company.pay_employees()
+
+main()
