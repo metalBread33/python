093b41124a5a11ba07be6871da460452244459c4
Kevin Foughty <97193605+metalBread33@users.noreply.github.com>
2024-10-02 14:58:49 +0000

    folder cleanup example

--- demos/fileCleanupDemo/cleanup.py
+++ demos/fileCleanupDemo/cleanup.py
@@ -1,4 +1,33 @@
 import os
 
+def move_file(destination_folder):
+    os.rename(
+            os.path.join(og_folder, file.name),
+            os.path.join(destination_folder, file.name))
+
 og_folder = 'demos/fileCleanupDemo'
-destination_folder = 'demos/fileCleanupDemo/CleanedUp'
+archived_folder = 'demos/fileCleanupDemo/CleanedUp/archived'
+document_folder = 'demos/fileCleanupDemo/CleanedUp/docs'
+picture_folder = 'demos/fileCleanupDemo/CleanedUp/pics'
+
+if not os.path.exists(archived_folder):
+    os.mkdir(archived_folder)
+
+if not os.path.exists(document_folder):
+    os.mkdir(document_folder)
+
+if not os.path.exists(picture_folder):
+    os.mkdir(picture_folder)
+
+for file in os.scandir(og_folder):
+    if os.path.splitext(file.name)[1] == '.txt':
+        print("Moving file", file.name)
+        move_file(document_folder)
+        
+    if os.path.splitext(file.name)[1] == '.zip':
+        print("Moving file", file.name)
+        move_file(archived_folder)
+
+    if os.path.splitext(file.name)[1] == '.png':
+        print("Moving file", file.name)
+        move_file(picture_folder)
