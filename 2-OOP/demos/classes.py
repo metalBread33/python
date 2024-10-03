9555674c4f579e59d4d7f78ad49eafbf7eecd87b
Kevin Foughty <97193605+metalBread33@users.noreply.github.com>
2024-10-03 11:15:03 +0000

    Getter and setters

--- 2-OOP/demos/classes.py
+++ 2-OOP/demos/classes.py
@@ -1,19 +1,40 @@
 class Employee:
-    def __init__(self, name, age):
+    def __init__(self, name, age, salary):
         self.__dict__['name'] = name
         self.age = age
+        self.salary = salary
     
     def printInfo(self):
-        print(self.name, 'is', self.age, 'years old')
+        print(self.name, 'is', self.age, 'years old and makes $', self.salary)
     
     def __str__(self):
         return f"I am printing {self.name}'s info"
     
     def __repr__(self):
-        return f"Employee({repr(self.name)}, {repr(self.age)})"
+        return f"Employee({repr(self.name)}, {repr(self.age)}, {repr(self.salary)})"
+    
+    def get_salary(self):
+        return self.salary
+    
+    def set_salary(self, salary):
+        #not pythonic way
+        if salary < 1000:
+            raise ValueError('Minimum wage is $1000') #validates salary
 
-e = Employee('kev', 22)
+e = Employee('kev', 22, 72561)
 print (e)
 print(e.__dict__)  
 e.printInfo()
 print(e.__repr__())
+
+try:
+    print("Setting salary to $1000")
+    e.set_salary(1000)
+except:
+    print("Can't set salary at or below $1000")
+
+print(e.get_salary())
+
+print("Setting salary to 20000")
+e.set_salary(20000)
+print(e.get_salary())
