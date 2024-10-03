adee8375718ef24a3cc61345c5ea5304039852f7
Kevin Foughty <97193605+metalBread33@users.noreply.github.com>
2024-10-03 12:07:28 +0000

    computed properties"

--- 2-OOP/demos/classes.py
+++ 2-OOP/demos/classes.py
@@ -5,6 +5,7 @@ class Employee:
         #self.set_salary(salary)
         self.salary = salary #properties provide validation
         self.__ssn = SSN
+        self.__monthly = None
 
     
     def printInfo(self):
@@ -19,17 +20,17 @@ class Employee:
     def get_salary(self):
         return self.__salary
     
-    #property getter
-    @property
-    def salary(self):
-        return self.__salary
-    
     def set_salary(self, salary):
         #not pythonic way
         if salary <= 1000:
             raise ValueError('Minimum wage is $1000') #validates salary
         else: 
             self.__salary = salary
+
+    #property getter
+    @property
+    def salary(self):
+        return self.__salary
 
     #property setter
     @salary.setter
@@ -38,6 +39,7 @@ class Employee:
             raise ValueError('Minimum wage is $1000') #validates salary
         else: 
             self.__salary = salary
+            self.__monthly = None
 
     #read only example
     @property
@@ -57,12 +59,21 @@ class Employee:
     def ssn(self, SSN):
         self.__ssn = SSN
 
+    @property
+    def monthlySalary(self):
+        if self.__monthly is None:
+            print("calculating monthly salary")
+            self.__monthly = self.__salary / 12
+        return self.__monthly
+
 
 e = Employee('kev', 22, 72561, '0')
 print (e)
 print(e.__dict__)  
 e.printInfo()
 print(e.__repr__())
+print("employee's monthly salary is:", e.monthlySalary)
+print("employee's monthly salary is:", e.monthlySalary)
 
 try:
     print("Setting salary to $1000")
@@ -73,9 +84,14 @@ except:
 print(e.get_salary())
 
 print("Setting salary to 20000")
-e.set_salary(20000)
+e.salary = 20000
 print(e.get_salary())
+
+print("employee's monthly salary is:", e.monthlySalary)
+print("employee's monthly salary is:", e.monthlySalary)
+
 print("printing from property", e.salary)
+
 
 #read only attribute example
 print(e.readOnly)
